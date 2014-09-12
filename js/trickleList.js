68750dce6bc71ec3f36497ee994cb2d6e8be03d2
Shaun Baer <sbaer@j2designpartnership.com>
2014-09-12 14:00:01 -0400

    - Initial Demo Files

---
+++ js/trickleList.js
@@ -0,0 +1,54 @@
+;(function($) {
+
+  $.fn.trickleList = function() {
+
+    var winWidth = $(window).width();
+
+    // Setup Vars
+    var options = $.extend({
+      childTarget: $(this),
+      delayInc: 150,
+      fadeTime: 400,
+      minWidth: 0
+    });
+
+    // Function for revealing children of element
+    function revealItem(){
+      var winWidth = $(window).width();
+      if(winWidth > options.minWidth){
+        var delayTime = 0;
+        $(options.childTarget).children().each(function(){
+          $(this).stop(true, false).delay(delayTime).fadeIn(options.fadeTime);
+          delayTime = delayTime + options.delayInc;
+        });
+      }
+    }
+
+    // Function for hiding children of element
+    function hideItem(){
+      var winWidth = $(window).width();
+      if(winWidth > options.minWidth){
+        $(options.childTarget).children().each(function(){
+          $(this).stop(true, false).fadeOut(options.fadeTime);
+        });
+      }
+    }
+
+    // Hide the Elements if needed
+    function windowCheck() {
+      if(winWidth > options.minWidth){
+        $(options.childTarget).children().hide();
+      }
+    }
+
+    windowCheck();
+
+    $(this).hover(revealItem, hideItem);
+
+    $(window).resize(function(){
+      winWidth = $(window).width();
+      windowCheck();
+    });
+  }
+
+}(jQuery));
